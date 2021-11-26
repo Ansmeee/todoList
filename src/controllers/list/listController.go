e17c53be84dacaeabe20d675ebe880afc481be31
wenjun zheng <wenjunzheng@sohu-inc.com>
2021-11-26 14:51:09 +0800

    TODO List 增加 list 相关接口

--- src/controllers/list/listController.go
+++ src/controllers/list/listController.go
@@ -38,3 +38,64 @@ func (ListController) Create(request *gin.Context) {
 
 	response.SuccessWithData(data)
 }
+
+func (ListController) Update(request *gin.Context)  {
+	response := response.Response{request}
+	var error error
+
+	data := service.NewModel()
+	error = request.ShouldBind(data)
+	if error != nil {
+		response.ErrorWithMSG("保存失败了，再试一次吧")
+		return
+	}
+
+	validator := new(lv.ListValidator)
+	error = validator.Validate(*data, lv.CreateRules)
+	if error != nil {
+		response.ErrorWithMSG(fmt.Sprintf("保存失败了，%s", error.Error()))
+		return
+	}
+
+	list, error := service.FindByID(data.Id)
+	if error != nil {
+		response.ErrorWithMSG("保存失败")
+		return
+	}
+
+	list, error = service.Update(list, data)
+	if error != nil {
+		response.ErrorWithMSG("保存失败")
+		return
+	}
+
+	response.SuccessWithData(*list)
+}
+
+func (ListController) Delete(request *gin.Context)  {
+	response := response.Response{request}
+	var error error
+
+	form := service.NewModel()
+	error = request.ShouldBindUri(form)
+	if error != nil {
+		response.ErrorWithMSG("删除失败")
+		return
+	}
+
+	list, error := service.FindByID(form.Id)
+	if error != nil {
+		response.ErrorWithMSG("删除失败")
+		return
+	}
+
+	error = service.Delete(list)
+
+	if error != nil {
+		response.ErrorWithMSG("删除失败")
+		return
+	}
+
+	response.SuccessWithMSG("删除成功")
+	return
+}
--- src/services/list/listService.go
+++ src/services/list/listService.go
@@ -1,6 +1,7 @@
 package list
 
 import (
+	"errors"
 	"todoList/src/models/list"
 	"todoList/src/utils/database"
 )
@@ -10,6 +11,18 @@ var model = &list.ListModel{}
 
 func (ListService) NewModel() *list.ListModel {
 	return new(list.ListModel)
+}
+
+func (ListService) FindByID(id uint) (list *list.ListModel, error error) {
+	db := database.Connect("")
+	defer database.Close(db)
+
+	error = db.Model(model).Find(list, id).Error
+	if error != nil {
+		return
+	}
+
+	return
 }
 
 func (ListService) Create(list *list.ListModel) (data *list.ListModel, error error) {
@@ -22,5 +35,36 @@ func (ListService) Create(list *list.ListModel) (data *list.ListModel, error err
 	}
 
 	data = list
+	return
+}
+
+func (ListService) Update(list, data *list.ListModel) (result *list.ListModel, error error) {
+	db := database.Connect("")
+	defer database.Close(db)
+
+	if list.Id == 0 {
+		error = errors.New("清单不存在")
+		return
+	}
+
+	error = db.Model(list).Updates(data).Error
+	if error != nil {
+		return
+	}
+
+	result = list
+	return
+}
+
+func (ListService) Delete(list *list.ListModel) (error error) {
+	db := database.Connect("")
+	defer database.Close(db)
+
+	if list.Id == 0 {
+		error = errors.New("清单不存在")
+		return
+	}
+
+	error = db.Model(model).Delete(list).Error
 	return
 }
